d0dba066a9ffd2029b37d2cd4c49dba0f60ab4ca
Jeanie Oh <51432553+jeanie-oh@users.noreply.github.com>
2025-04-22 18:21:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ st.markdown("<h3 style='text-align: center; primaryColor: white; secondaryColor:
 
 country_list =['Brazil', 'India', 'Thailand','Vietnam', 'Indonesia','China', 'Malaysia','EU', 'Japan','South Korea', 'Canada','Israel', 'Colombia','Mexico', 'Singapore']
 
-country_list.append('Simple_Average','Country Charging The Tariff')
-print(country_list)
+# country_list.append('Simple_Average','Country Charging The Tariff')
+print(type(country_list))
 
 countries_picked = st_tags(
     label='Enter Keywords:',
