ab606cecd7cd151e2695bdc7c44e109e46ddb173
Jeanie Oh <51432553+jeanie-oh@users.noreply.github.com>
2025-04-22 19:18:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -69,13 +69,16 @@ fig.update_layout(legend=dict(
 # fig.update_layout(legend_traceorder="reversed")
 st.plotly_chart(fig)
 
+def_url='https://wits.worldbank.org/Bilateral-Tariff-Technical-Note.html'
+
 st.caption("""
         1. Simple Applied Average figures were used. The Simple Applied Average was preferred to the MFN Simple Average because it accounts for trade agreements in place between countries.
-           The Simple Applied Average was preferred to the Weighted Applied Average because the Weighted Applied Average incorporates trade volume which is driven by consumer behavior and cannot be directly controlled by government. The Simple Average summarises tariff rates that goverments charge.
+           The Simple Applied Average was preferred to the Weighted Applied Average because the Weighted Applied Average incorporates trade volume which is driven by consumer behavior and cannot be directly controlled by government. 
+           The Simple Applied Average appears to be the closest approximation of tariff rates that goverments charge in this context. Detailed definitions available on [this link](%s).
         2. Year 2022 figures were used because this was the most recent applied simple average tariff rate computation found on WITS.
            WTO and more recent data sources that provide 2023 and 2024 figures do not compute an applied simple average tariff.<br> 
         3. Current US tariff summary average is based on [the BBC](%s) figures released April 10, 2025.<br>
-        4. US current tariff to China is 140 percent and China's retaliation rate is 125 percent. This is not depicted in the graph as it is too large, instead, it is noted in the below table"""% url, unsafe_allow_html=True)
+        4. US current tariff to China is 140 percent and China's retaliation rate is 125 percent. This is not depicted in the graph as it is too large, instead, it is noted in the below table"""%def_url % url, unsafe_allow_html=True)
 
 
 data_before_table=pd.read_csv('BeforeTrump_Table.csv')
