3ea6a633d1b09282ac8bef4187e51a2cbd006e55
Jeanie Oh <51432553+jeanie-oh@users.noreply.github.com>
2025-04-22 19:43:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ data_before_bar['Simple_Average']=data_before_bar['Simple_Average']*100
 
 url='https://wits.worldbank.org/'
 st.caption("Based on data from [WITS](%s). The latest comprehensive data that provides 'Simple Applied Average Rates' prior to the Trump tariffs is 2022. " \
-"The purpose of this data is to provide information around how much each government charged in tariffs to the United States before and after April 11, 2025." % url)
+"The purpose of this data is to provide information around how much each government charged in tariffs to the United States before and after April 11, 2025. We chose this date because the Trump administration" \
+"imposed their tariffs on April 10th and the following day (April 11th) includes retaliation tariff rates of US trade partners." % url)
 
 # BEFORE AND AFTER TRUMP ADMINISTRATION
 # st.markdown("<h3 style='text-align: center; primaryColor: white; secondaryColor: black;' >" \
@@ -74,7 +75,7 @@ st.caption("""
            The Simple Applied Average was preferred to the Weighted Applied Average because the Weighted Applied Average incorporates trade volume which is driven by consumer behavior and cannot be directly controlled by government. 
            The Simple Applied Average appears to be the closest approximation of tariff rates that goverments charge in this context. Detailed definitions available on <a href='https://wits.worldbank.org/Bilateral-Tariff-Technical-Note.html'>this link</a>.
         2. Year 2022 figures were used because this was the most recent applied simple average tariff rate computation found on WITS.
-           WTO and more recent data sources that provide 2023 and 2024 figures do not compute an applied simple average tariff.<br> 
+           WTO and more recent data sources that provide 2023 and 2024 figures do not compute a Simple Applied Average Rate for tariffs.<br> 
         3. Current US tariff summary average is based on <a href='https://www.bbc.com/news/articles/c5ypxnnyg7jo'>the BBC</a> figures released April 10, 2025.<br>
         4. US current tariff to China is 140 percent and China's retaliation rate is 125 percent. This is not depicted in the graph as it is too large, instead, it is noted in the below table""" , unsafe_allow_html=True)
 
